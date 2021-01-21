dd19b54cb53b369df489a691d210458950646ba9
jeff <jeanfrancois.roussel@corum-am.com>
2021-01-21 18:06:30 +0100

    add default export

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import styles from './styles.module.css'
 
-export const CorumButton = ({ text }) => {
+const CorumButton = ({ text }) => {
   return <div className={styles.test}>Message: {text}</div>
 }
+
+export default CorumButton
